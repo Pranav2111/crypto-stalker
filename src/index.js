dd9a33d9ea1807a147dcd556f4036748b5bf1851
Pranav Patil <pranavpatil@Pranavs-MacBook-Air.local>
2022-04-07 18:05:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter  } from "react-router-dom";
 import ContextCryto from './contextApi/ContextCryto';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ContextCryto>
         <App />
       </ContextCryto>
